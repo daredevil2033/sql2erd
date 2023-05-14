cc851f07b4ddc488d8802a56943cee1c6a208faf
daredevil2033 <zulyukovivan@gmail.com>
2023-05-14 17:36:56 +0300

    refactor controllers

--- src/main/java/edu/sumdu/dbis/sql2erd/controller/FileController.java
+++ src/main/java/edu/sumdu/dbis/sql2erd/controller/FileController.java
@@ -30,75 +30,83 @@ public class FileController {
                 CreateTable createTable = (CreateTable) statement;
                 String tableName = createTable.getTable().getName();
                 Table table = new Table(tableName);
-                List<ColumnDefinition> columnDefinitions = createTable.getColumnDefinitions();
-                for (ColumnDefinition columnDefinition : columnDefinitions) {
-                    Column column = new Column(columnDefinition.getColumnName(), columnDefinition.getColDataType().toString());
-                    List<String> columnSpecs = columnDefinition.getColumnSpecs();
-                    if (columnSpecs != null) {
-                        for (String columnSpec : columnSpecs) {
-                            if (columnSpec.equalsIgnoreCase("PRIMARY") &&
-                                    columnSpecs.get(columnSpecs.indexOf(columnSpec) + 1).equalsIgnoreCase("KEY")) {
-                                column.setPrimaryKey(true);
-                                column.setNotNull(true);
-                                column.setUnique(true);
-                            } else if (columnSpec.equalsIgnoreCase("NOT") &&
-                                    columnSpecs.get(columnSpecs.indexOf(columnSpec) + 1).equalsIgnoreCase("NULL")) {
-                                column.setNotNull(true);
-                            } else if (columnSpec.equalsIgnoreCase("UNIQUE")) {
-                                column.setUnique(true);
-                            } else if (columnSpec.equalsIgnoreCase("REFERENCES")) {
-                                column.setForeignKey(true);
-                                String referencedColumnName = columnSpecs.get(columnSpecs.indexOf(columnSpec) + 2);
-                                Reference reference = new Reference(Collections.singletonList(column.getName()),
-                                        columnSpecs.get(columnSpecs.indexOf(columnSpec) + 1),
-                                        Collections.singletonList(referencedColumnName.substring(1, referencedColumnName.length() - 1)));
-                                if (columnSpecs.indexOf(columnSpec) - 2 >= 0) {
-                                    if (columnSpecs.get(columnSpecs.indexOf(columnSpec) - 2).equalsIgnoreCase("CONSTRAINT")) {
-                                        reference.setName(columnSpecs.get(columnSpecs.indexOf(columnSpec) - 1));
-                                    }
-                                }
-                                table.addReference(reference);
-                            }
-                        }
-                    }
-                    table.addColumn(column);
-                }
-                List<Index> indices = createTable.getIndexes();
-                if (indices != null) {
-                    for (Index index : indices) {
-                        String indexName = index.getName();
-                        Map<String, Column> columnMap = table.getColumns().stream().collect(Collectors.toMap(Column::getName, Function.identity()));
-                        if (index instanceof ForeignKeyIndex) {
-                            ForeignKeyIndex fkIndex = (ForeignKeyIndex) index;
-                            for (String columnName : fkIndex.getColumnsNames()) {
-                                columnMap.get(columnName).setForeignKey(true);
-                            }
-                            Reference reference = new Reference(fkIndex.getColumnsNames(), fkIndex.getTable().getName(), fkIndex.getReferencedColumnNames());
-                            reference.setName(indexName);
-                            table.addReference(reference);
-                        } else {
-                            List<String> columnNames = index.getColumnsNames();
-                            for (String columnName : columnNames) {
-                                Column column = columnMap.get(columnName);
-                                if (columnNames.size() == 1) {
-                                    if (index.getType().equalsIgnoreCase("UNIQUE")) {
-                                        column.setUnique(true);
-                                    }
-                                }
-                                if (index.getType().equalsIgnoreCase("PRIMARY KEY")) {
-                                    column.setPrimaryKey(true);
-                                    column.setNotNull(true);
-                                    if (columnNames.size() == 1) {
-                                        column.setUnique(true);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                getColumns(createTable, table);
+                getIndices(createTable, table);
                 tables.add(table);
             }
         }
         return tables;
+    }
+
+    private static void getColumns(CreateTable createTable, Table table) {
+        List<ColumnDefinition> columnDefinitions = createTable.getColumnDefinitions();
+        for (ColumnDefinition columnDefinition : columnDefinitions) {
+            Column column = new Column(columnDefinition.getColumnName(), columnDefinition.getColDataType().toString());
+            List<String> columnSpecs = columnDefinition.getColumnSpecs();
+            if (columnSpecs != null) {
+                for (String columnSpec : columnSpecs) {
+                    if (columnSpec.equalsIgnoreCase("PRIMARY") &&
+                            columnSpecs.get(columnSpecs.indexOf(columnSpec) + 1).equalsIgnoreCase("KEY")) {
+                        column.setPrimaryKey(true);
+                        column.setNotNull(true);
+                        column.setUnique(true);
+                    } else if (columnSpec.equalsIgnoreCase("NOT") &&
+                            columnSpecs.get(columnSpecs.indexOf(columnSpec) + 1).equalsIgnoreCase("NULL")) {
+                        column.setNotNull(true);
+                    } else if (columnSpec.equalsIgnoreCase("UNIQUE")) {
+                        column.setUnique(true);
+                    } else if (columnSpec.equalsIgnoreCase("REFERENCES")) {
+                        column.setForeignKey(true);
+                        String referencedColumnName = columnSpecs.get(columnSpecs.indexOf(columnSpec) + 2);
+                        Reference reference = new Reference(Collections.singletonList(column.getName()),
+                                columnSpecs.get(columnSpecs.indexOf(columnSpec) + 1),
+                                Collections.singletonList(referencedColumnName.substring(1, referencedColumnName.length() - 1)));
+                        if (columnSpecs.indexOf(columnSpec) - 2 >= 0) {
+                            if (columnSpecs.get(columnSpecs.indexOf(columnSpec) - 2).equalsIgnoreCase("CONSTRAINT")) {
+                                reference.setName(columnSpecs.get(columnSpecs.indexOf(columnSpec) - 1));
+                            }
+                        }
+                        table.addReference(reference);
+                    }
+                }
+            }
+            table.addColumn(column);
+        }
+    }
+
+    private static void getIndices(CreateTable createTable, Table table) {
+        List<Index> indices = createTable.getIndexes();
+        if (indices != null) {
+            for (Index index : indices) {
+                String indexName = index.getName();
+                Map<String, Column> columnMap = table.getColumns().stream().collect(Collectors.toMap(Column::getName, Function.identity()));
+                if (index instanceof ForeignKeyIndex) {
+                    ForeignKeyIndex fkIndex = (ForeignKeyIndex) index;
+                    for (String columnName : fkIndex.getColumnsNames()) {
+                        columnMap.get(columnName).setForeignKey(true);
+                    }
+                    Reference reference = new Reference(fkIndex.getColumnsNames(), fkIndex.getTable().getName(), fkIndex.getReferencedColumnNames());
+                    reference.setName(indexName);
+                    table.addReference(reference);
+                } else {
+                    List<String> columnNames = index.getColumnsNames();
+                    for (String columnName : columnNames) {
+                        Column column = columnMap.get(columnName);
+                        if (columnNames.size() == 1) {
+                            if (index.getType().equalsIgnoreCase("UNIQUE")) {
+                                column.setUnique(true);
+                            }
+                        }
+                        if (index.getType().equalsIgnoreCase("PRIMARY KEY")) {
+                            column.setPrimaryKey(true);
+                            column.setNotNull(true);
+                            if (columnNames.size() == 1) {
+                                column.setUnique(true);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/edu/sumdu/dbis/sql2erd/controller/LiveController.java
+++ src/main/java/edu/sumdu/dbis/sql2erd/controller/LiveController.java
@@ -18,71 +18,87 @@ public class LiveController {
         while (tables.next()) {
             String tableName = tables.getString("TABLE_NAME");
             Table table = new Table(tableName);
-            ResultSet columns = metaData.getColumns(catalogName, schemaName, tableName, null);
-            while (columns.next()) {
-                Column column;
-                int colSize = columns.getInt("COLUMN_SIZE");
-                if (colSize == Integer.MAX_VALUE)
-                    column = new Column(columns.getString("COLUMN_NAME"), columns.getString("TYPE_NAME"));
-                else column = new Column(columns.getString("COLUMN_NAME"),
-                        columns.getString("TYPE_NAME") + " (" + colSize + ")");
-                if (columns.getString("IS_NULLABLE").equals("NO")) {
-                    column.setNotNull(true);
-                }
-                table.addColumn(column);
-            }
-            ResultSet indices = metaData.getIndexInfo(catalogName, schemaName, tableName, true, false);
+            getColumns(catalogName, schemaName, metaData, tableName, table);
             Map<String, Column> columnMap = table.getColumns().stream().collect(Collectors.toMap(Column::getName, Function.identity()));
-            String prevColumnName = null;
-            String currIndexName;
-            String currColumnName;
-            Set<String> indexHashSet = new HashSet<>();
-            while (indices.next()) {
-                currIndexName = indices.getString("INDEX_NAME");
-                currColumnName = indices.getString("COLUMN_NAME");
-                if (currIndexName != null) {
-                    if (!indexHashSet.contains(currIndexName)) {
-                        indexHashSet.add(currIndexName);
-                        columnMap.get(currColumnName).setUnique(true);
-                        prevColumnName = currColumnName;
-                    } else columnMap.get(prevColumnName).setUnique(false);
-                } else columnMap.get(currColumnName).setUnique(true);
+            getUniqueConstraints(catalogName, schemaName, metaData, tableName, columnMap);
+            getPrimaryKeys(catalogName, schemaName, metaData, tableName, columnMap);
+            getReferences(catalogName, schemaName, metaData, tableName, table, columnMap);
+            tableList.add(table);
+        }
+        return tableList;
+    }
+
+    private static void getColumns(String catalogName, String schemaName, DatabaseMetaData metaData, String tableName, Table table) throws SQLException {
+        ResultSet columns = metaData.getColumns(catalogName, schemaName, tableName, null);
+        while (columns.next()) {
+            Column column;
+            int colSize = columns.getInt("COLUMN_SIZE");
+            if (colSize == Integer.MAX_VALUE)
+                column = new Column(columns.getString("COLUMN_NAME"), columns.getString("TYPE_NAME"));
+            else column = new Column(columns.getString("COLUMN_NAME"),
+                    columns.getString("TYPE_NAME") + " (" + colSize + ")");
+            if (columns.getString("IS_NULLABLE").equals("NO")) {
+                column.setNotNull(true);
             }
-            ResultSet primaryKeys = metaData.getPrimaryKeys(catalogName, schemaName, tableName);
-            while (primaryKeys.next()) {
-                columnMap.get(primaryKeys.getString("COLUMN_NAME")).setPrimaryKey(true);
-            }
-            ResultSet foreignKeys = metaData.getImportedKeys(catalogName, schemaName, tableName);
-            Reference reference = null;
-            String currReferencedTableName;
-            String prevReferencedTableName = null;
-            while (foreignKeys.next()) {
-                currReferencedTableName = foreignKeys.getString("PKTABLE_NAME");
-                if (currReferencedTableName.equals(prevReferencedTableName)) {
-                    if (foreignKeys.getShort("KEY_SEQ") == 1) {
-                        table.addReference(reference);
-                        reference = new Reference();
-                        reference.setName(foreignKeys.getString("FK_NAME"));
-                        reference.setReferencedTableName(currReferencedTableName);
-                    }
-                } else {
-                    if (reference != null) {
-                        table.addReference(reference);
-                    }
+            table.addColumn(column);
+        }
+    }
+
+    private static void getUniqueConstraints(String catalogName, String schemaName, DatabaseMetaData metaData, String tableName, Map<String, Column> columnMap) throws SQLException {
+        ResultSet indices = metaData.getIndexInfo(catalogName, schemaName, tableName, true, false);
+        String prevColumnName = null;
+        String currIndexName;
+        String currColumnName;
+        Set<String> indexHashSet = new HashSet<>();
+        while (indices.next()) {
+            currIndexName = indices.getString("INDEX_NAME");
+            currColumnName = indices.getString("COLUMN_NAME");
+            if (currIndexName != null) {
+                if (!indexHashSet.contains(currIndexName)) {
+                    indexHashSet.add(currIndexName);
+                    columnMap.get(currColumnName).setUnique(true);
+                    prevColumnName = currColumnName;
+                } else columnMap.get(prevColumnName).setUnique(false);
+            } else columnMap.get(currColumnName).setUnique(true);
+        }
+    }
+
+    private static void getPrimaryKeys(String catalogName, String schemaName, DatabaseMetaData metaData, String tableName, Map<String, Column> columnMap) throws SQLException {
+        ResultSet primaryKeys = metaData.getPrimaryKeys(catalogName, schemaName, tableName);
+        while (primaryKeys.next()) {
+            columnMap.get(primaryKeys.getString("COLUMN_NAME")).setPrimaryKey(true);
+        }
+    }
+
+    private static void getReferences(String catalogName, String schemaName, DatabaseMetaData metaData, String tableName, Table table, Map<String, Column> columnMap) throws SQLException {
+        ResultSet foreignKeys = metaData.getImportedKeys(catalogName, schemaName, tableName);
+        Reference reference = null;
+        String currReferencedTableName;
+        String prevReferencedTableName = null;
+        while (foreignKeys.next()) {
+            currReferencedTableName = foreignKeys.getString("PKTABLE_NAME");
+            if (currReferencedTableName.equals(prevReferencedTableName)) {
+                if (foreignKeys.getShort("KEY_SEQ") == 1) {
+                    table.addReference(reference);
                     reference = new Reference();
                     reference.setName(foreignKeys.getString("FK_NAME"));
                     reference.setReferencedTableName(currReferencedTableName);
                 }
-                columnMap.get(foreignKeys.getString("FKCOLUMN_NAME")).setForeignKey(true);
-                reference.addColumnName(foreignKeys.getString("FKCOLUMN_NAME"));
-                reference.addReferencedColumnName(foreignKeys.getString("PKCOLUMN_NAME"));
-                prevReferencedTableName = currReferencedTableName;
+            } else {
+                if (reference != null) {
+                    table.addReference(reference);
+                }
+                reference = new Reference();
+                reference.setName(foreignKeys.getString("FK_NAME"));
+                reference.setReferencedTableName(currReferencedTableName);
             }
-            if (reference != null) {
-                table.addReference(reference);
-            }
-            tableList.add(table);
+            columnMap.get(foreignKeys.getString("FKCOLUMN_NAME")).setForeignKey(true);
+            reference.addColumnName(foreignKeys.getString("FKCOLUMN_NAME"));
+            reference.addReferencedColumnName(foreignKeys.getString("PKCOLUMN_NAME"));
+            prevReferencedTableName = currReferencedTableName;
         }
-        return tableList;
+        if (reference != null) {
+            table.addReference(reference);
+        }
     }
 }
